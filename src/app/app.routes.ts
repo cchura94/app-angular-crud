d6195569990968eeaf184b6b5b40db2164d7b801
Cristian <cchura.cpc@gmail.com>
2026-07-02 21:39:39 -0400

    40

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Blog } from './blog/blog';
 import { Producto } from './producto/producto';
+import { Error404 } from './error404/error404';
 
 export const routes: Routes = [
     {path: '', component: Blog},
-    {path: 'productos', component: Producto}
+    {path: 'productos', component: Producto},
+    {path: '**', component: Error404}
 
 ];
